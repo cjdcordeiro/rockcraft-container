b23c9cd2ce1ba90844366e29cf68b0c66cbd544c
Cristovao Cordeiro <cristovao.cordeiro@canonical.com>
2023-02-10 17:54:37 +0100

    fix: remove unused code

--- entrypoint-wrapper.sh
+++ entrypoint-wrapper.sh
@@ -4,8 +4,4 @@ args="$@"
 
 sed -i "0,/REPLACEME/ s/REPLACEME/${args}/" /usr/local/bin/entrypoint.sh
 
-touch /tmp/log
-
-tail -f /tmp/log 2>&1 &
-
 exec /lib/systemd/systemd --system --system-unit entrypoint.service --show-status=true
